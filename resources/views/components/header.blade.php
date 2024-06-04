86c9740c6becf398884ba2c3e86a159311e2c119
simadmin <cimahi@TTUC7>
2024-06-04 13:07:36 +0700

    set responsive for header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -3,8 +3,8 @@
     <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
       <div class="lg:pr-8 lg:pt-4">
         <div class="lg:max-w-lg">
-        <h2 class="lg:text-6xl sm:text-6xl font-bold leading-30 text-white">Layanan Informasi dan Pengaduan Masyarakat</h2>
-          <p class="text-white font-medium mt-7">Bersih Melayani Taat Integritas</p>
+          <h2 class="text-2xl sm:text-3xl md:text-4xl lg:text-6xl font-bold leading-30 text-white text-center lg:text-left">Layanan Informasi dan Pengaduan Masyarakat</h2>
+          <p class="text-sm sm:text-lg md:text-xl lg:text-base text-white font-medium mt-7 text-center lg:text-left">Bersih Melayani Taat Integritas</p>
         </div>
       </div>
       <img src="{{ asset('img/ILUSTRASI CUSTOMER SERVICE.png') }}" alt="Product screenshot" class="float-animation" width="2432" height="1442">
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -36,7 +36,7 @@
   </div>
 </nav>
 <!-- Mobile menu, show/hide based on menu open state. -->
-<div class="lg:hidden" role="dialog" aria-modal="true">
+{{-- <div class="lg:hidden" role="dialog" aria-modal="true">
   <!-- Background backdrop, show/hide based on slide-over state. -->
   <div class="fixed inset-0 z-50"></div>
   <div class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
@@ -66,4 +66,4 @@
       </div>
     </div>
   </div>
-</div>
+</div> --}}
