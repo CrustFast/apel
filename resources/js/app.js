4853f0ecc98c90d1b72856118557ed572b4f4501
simadmin <cimahi@TTUC7>
2024-06-05 14:38:08 +0700

    add select date input using flowbite

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,6 @@ import './bootstrap';
 import 'flowbite';
 import flatpickr from "flatpickr";
 import Datepicker from 'flowbite-datepicker/Datepicker';
-<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+{/* <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script> */}
 
 
