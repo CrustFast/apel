cb230f7d91116e99ce50ba6393b7e7817404fb3c
simadmin <cimahi@TTUC7>
2024-06-07 21:12:55 +0700

    add: add content in main page

--- resources/views/components/apel.blade.php
+++ resources/views/components/apel.blade.php
@@ -59,7 +59,7 @@
     </main>
 
     <section>
-        <div class="flex justify-center mt-20">
+        <div class="flex justify-center mt-40 mb-16">
             <div class="max-w-7xl px-6 lg:px-6">
                 <div class="grid max-w-2xl grid-cols-1 gap-x-52 gap-y-16 sm:gap-y-20 lg:max-w-none lg:grid-cols-2">
                     <div
@@ -101,6 +101,67 @@
         </div>
     </section>
 
+    <div class="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
+      <div class="absolute inset-0 -z-10 overflow-hidden">
+      </div>
+      <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
+        <div class="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+          <div class="lg:pr-4">
+            <div class="lg:max-w-lg">
+              <p class="text-base font-semibold leading-7 text-blue-bmti">Layanan Pengaduan Internal dan Eksternal BBPPMPV BMTI</p>
+              <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Suara Anda, Perubahan Kami!</h1>
+              <h2 class="mt-16 text-2xl font-bold tracking-tight text-gray-900">Apa itu Pengaduan Masyarakat?</h2>
+              <p class="mt-4 text-gray-700">Pengaduan masyarakat adalah segala bentuk informasi atau komunikasi berupa pengaduan yang disampaikan oleh masyarakat, termasuk pegawai BBPPMPV BMTI yang bertindak sebagai masyarakat umum ketika menggunakan layanan kami. Pengaduan ini mencakup berbagai jenis layanan yang ada di BBPPMPV BMTI.</p>
+            </div>
+          </div>
+        </div>
+        <div class="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
+          <img width="2432" height="1442" src="{{ asset('img/salam.png') }}" alt="">
+        </div>
+        <div class="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+          <div class="lg:pr-4">
+            <div class="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg">
+              <h2 class="my-3 text-2xl font-bold tracking-tight text-gray-900">Mengapa Pengaduan Penting?</h2>
+              <p class="mt-4 text-gray-700">Pengaduan masyarakat bukan hanya sekadar sarana untuk menyampaikan keluhan atau masalah, tetapi juga merupakan instrumen penting untuk mendorong perbaikan dan perubahan positif di dalam organisasi. Dengan menyampaikan masalah atau kekurangan yang teridentifikasi, manajemen BBPPMPV BMTI dapat mengambil langkah-langkah yang diperlukan untuk memperbaiki sistem atau prosedur yang ada. Ini membantu meningkatkan efisiensi dan efektivitas kerja, serta memperkuat hubungan antara organisasi dengan stakeholder-nya.</p>
+              <h2 class="mt-8 text-2xl font-bold tracking-tight text-gray-900">Manfaat Pengaduan Masyarakat:</h2>
+              <ul role="list" class="mt-8 space-y-8 text-gray-600">
+                <li class="flex gap-x-3">
+                  <svg class="mt-1 h-5 w-5 flex-none text-blue-bmti" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                    <path fill-rule="evenodd" d="M12 6.75a5.25 5.25 0 0 1 6.775-5.025.75.75 0 0 1 .313 1.248l-3.32 3.319c.063.475.276.934.641 1.299.365.365.824.578 1.3.64l3.318-3.319a.75.75 0 0 1 1.248.313 5.25 5.25 0 0 1-5.472 6.756c-1.018-.086-1.87.1-2.309.634L7.344 21.3A3.298 3.298 0 1 1 2.7 16.657l8.684-7.151c.533-.44.72-1.291.634-2.309A5.342 5.342 0 0 1 12 6.75ZM4.117 19.125a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75h-.008a.75.75 0 0 1-.75-.75v-.008Z" clip-rule="evenodd" />
+                    <path d="m10.076 8.64-2.201-2.2V4.874a.75.75 0 0 0-.364-.643l-3.75-2.25a.75.75 0 0 0-.916.113l-.75.75a.75.75 0 0 0-.113.916l2.25 3.75a.75.75 0 0 0 .643.364h1.564l2.062 2.062 1.575-1.297Z" />
+                    <path fill-rule="evenodd" d="m12.556 17.329 4.183 4.182a3.375 3.375 0 0 0 4.773-4.773l-3.306-3.305a6.803 6.803 0 0 1-1.53.043c-.394-.034-.682-.006-.867.042a.589.589 0 0 0-.167.063l-3.086 3.748Zm3.414-1.36a.75.75 0 0 1 1.06 0l1.875 1.876a.75.75 0 1 1-1.06 1.06L15.97 17.03a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
+                  </svg> 
+                  <span><strong class="font-semibold text-gray-900">Perbaikan Sistem dan Prosedur:</strong> Dengan melaporkan masalah, manajemen dapat mengidentifikasi dan memperbaiki kekurangan dalam sistem atau prosedur, sehingga meningkatkan kualitas layanan.</span>
+                </li>
+                <li class="flex gap-x-3">
+                  <svg class="mt-1 h-5 w-5 flex-none text-blue-bmti" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                    <path fill-rule="evenodd" d="M7.502 6h7.128A3.375 3.375 0 0 1 18 9.375v9.375a3 3 0 0 0 3-3V6.108c0-1.505-1.125-2.811-2.664-2.94a48.972 48.972 0 0 0-.673-.05A3 3 0 0 0 15 1.5h-1.5a3 3 0 0 0-2.663 1.618c-.225.015-.45.032-.673.05C8.662 3.295 7.554 4.542 7.502 6ZM13.5 3A1.5 1.5 0 0 0 12 4.5h4.5A1.5 1.5 0 0 0 15 3h-1.5Z" clip-rule="evenodd" />
+                    <path fill-rule="evenodd" d="M3 9.375C3 8.339 3.84 7.5 4.875 7.5h9.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 0 1 3 20.625V9.375Zm9.586 4.594a.75.75 0 0 0-1.172-.938l-2.476 3.096-.908-.907a.75.75 0 0 0-1.06 1.06l1.5 1.5a.75.75 0 0 0 1.116-.062l3-3.75Z" clip-rule="evenodd" />
+                  </svg>                                                 
+                  <span><strong class="font-semibold text-gray-900">Efisiensi dan Efektivitas:</strong> Pengaduan membantu mengoptimalkan proses kerja dan meningkatkan kinerja organisasi.</span>
+                </li>
+                <li class="flex gap-x-3">
+                  <svg class="mt-1 h-5 w-5 flex-none text-blue-bmti" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                    <path fill-rule="evenodd" d="M8.25 6.75a3.75 3.75 0 1 1 7.5 0 3.75 3.75 0 0 1-7.5 0ZM15.75 9.75a3 3 0 1 1 6 0 3 3 0 0 1-6 0ZM2.25 9.75a3 3 0 1 1 6 0 3 3 0 0 1-6 0ZM6.31 15.117A6.745 6.745 0 0 1 12 12a6.745 6.745 0 0 1 6.709 7.498.75.75 0 0 1-.372.568A12.696 12.696 0 0 1 12 21.75c-2.305 0-4.47-.612-6.337-1.684a.75.75 0 0 1-.372-.568 6.787 6.787 0 0 1 1.019-4.38Z" clip-rule="evenodd" />
+                    <path d="M5.082 14.254a8.287 8.287 0 0 0-1.308 5.135 9.687 9.687 0 0 1-1.764-.44l-.115-.04a.563.563 0 0 1-.373-.487l-.01-.121a3.75 3.75 0 0 1 3.57-4.047ZM20.226 19.389a8.287 8.287 0 0 0-1.308-5.135 3.75 3.75 0 0 1 3.57 4.047l-.01.121a.563.563 0 0 1-.373.486l-.115.04c-.567.2-1.156.349-1.764.441Z" />
+                  </svg>                  
+                  <span><strong class="font-semibold text-gray-900">Hubungan yang Lebih Kuat dengan Stakeholder:</strong> Menanggapi pengaduan dengan cepat dan efektif dapat memperkuat kepercayaan dan hubungan antara BBPPMPV BMTI dan para pemangku kepentingannya.</span>
+                </li>
+                <li class="flex gap-x-3">
+                  <svg class="mt-1 h-5 w-5 flex-none text-blue-bmti" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                    <path d="M18.75 12.75h1.5a.75.75 0 0 0 0-1.5h-1.5a.75.75 0 0 0 0 1.5ZM12 6a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5h-7.5A.75.75 0 0 1 12 6ZM12 18a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5h-7.5A.75.75 0 0 1 12 18ZM3.75 6.75h1.5a.75.75 0 1 0 0-1.5h-1.5a.75.75 0 0 0 0 1.5ZM5.25 18.75h-1.5a.75.75 0 0 1 0-1.5h1.5a.75.75 0 0 1 0 1.5ZM3 12a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5h-7.5A.75.75 0 0 1 3 12ZM9 3.75a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5ZM12.75 12a2.25 2.25 0 1 1 4.5 0 2.25 2.25 0 0 1-4.5 0ZM9 15.75a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5Z" />
+                  </svg>                  
+                  <span><strong class="font-semibold text-gray-900">Tata Kelola yang Baik:</strong> Pengaduan membantu memastikan bahwa BBPPMPV BMTI beroperasi dengan prinsip-prinsip transparansi, akuntabilitas, dan tanggung jawab.</span>
+                </li>
+              </ul>
+              <p class="mt-8">Mari bersama-sama kita ciptakan lingkungan kerja yang lebih baik dan layanan yang lebih unggul. Sampaikan pengaduan Anda sekarang dan jadilah bagian dari perubahan positif di BBPPMPV BMTI.</p>
+              <h2 class="mt-16 text-2xl font-bold tracking-tight text-gray-900">BBPPMPV BMTI - Bersih Melayani Taat Integritas</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  
     <footer>
         <section class="bg-white">
             <div class="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
