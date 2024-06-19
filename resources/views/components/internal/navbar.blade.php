d0107c44406a73c45ea13f94e9b247353bf5bddf
simadmin <cimahi@TTUC7>
2024-06-19 16:46:49 +0700

    add: menu and links in navbar

--- resources/views/components/internal/navbar.blade.php
+++ resources/views/components/internal/navbar.blade.php
@@ -28,91 +28,80 @@
       <span class="block h-1 w-0 bg-blue-bmti rounded-full mt-1 mx-auto transition-all duration-300 group-hover:w-10"></span>
     </a>
     <div class="relative" x-data="{ open: false }">
-      <button @click="open = !open" @click.away="open = false" type="button" class="inline-flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900" aria-expanded="false">
-        <span>Layanan Kami</span>
+      <button @mouseenter="open = true" @mouseleave="open = false" type="button" class="inline-flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900" aria-expanded="false">
+        <span>Pengendalian Gratifikasi</span>
         <svg :class="{'rotate-180': open}" class="h-5 w-5 flex-none text-gray-400 transition-transform" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
           <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
         </svg>
       </button>
 
-      <div x-show="open" x-transition:enter="transition ease-out duration-200"
-           x-transition:enter-start="opacity-0 translate-y-1"
-           x-transition:enter-end="opacity-100 translate-y-0"
-           x-transition:leave="transition ease-in duration-150"
-           x-transition:leave-start="opacity-100 translate-y-0"
-           x-transition:leave-end="opacity-0 translate-y-1"
-           class="absolute left-1/2 z-50 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4">
+      <div x-show="open" @mouseenter="open = true" @mouseleave="open = false" x-transition:enter="transition ease-out duration-200"
+          x-transition:enter-start="opacity-0 translate-y-1"
+          x-transition:enter-end="opacity-100 translate-y-0"
+          x-transition:leave="transition ease-in duration-150"
+          x-transition:leave-start="opacity-100 translate-y-0"
+          x-transition:leave-end="opacity-0 translate-y-1"
+          class="absolute left-1/2 z-50 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4">
         <div class="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
-          <div class="p-4">
+          <div class="p-4 max-h-96 overflow-y-auto custom-scrollbar">
             <div class="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
-              <div class="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                <svg class="h-6 w-6 text-gray-600 group-hover:text-indigo-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6a7.5 7.5 0 107.5 7.5h-7.5V6z" />
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 10.5H21A7.5 7.5 0 0013.5 3v7.5z" />
-                </svg>
+              <div class="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                <img class="h-6 w-6 group-hover:text-indigo-600" src="{{ asset('img/Logo E-Learning KPK.png') }}" alt="Logo">
               </div>
               <div>
-                <a href="#" class="font-semibold text-gray-900">
-                  Benturan Kepentingan
+                <a href="https://elearning.kpk.go.id/moodle/" target="_blank" class="font-semibold text-gray-900">
+                  E-learning Pengendalian Gratifikasi
                   <span class="absolute inset-0"></span>
                 </a>
-                <p class="mt-1 text-gray-600">Get a better understanding of your traffic</p>
+                <p class="mt-1 text-gray-600">Upaya KPK untuk memberikan edukasi dan sertifikasi tentang pengendalian gratifikasi.</p>
               </div>
             </div>
             <div class="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
-              <div class="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                <svg class="h-6 w-6 text-gray-600 group-hover:text-indigo-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M15.042 21.672L13.684 16.6m0 0l-2.51 2.225.569-9.47 5.227 7.917-3.286-.672zM12 2.25V4.5m5.834.166l-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243l-1.59-1.59" />
-                </svg>
+              <div class="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                <img class="h-6 w-6 group-hover:text-indigo-600" src="{{ asset('img/logo-jaga-new.png') }}" alt="Logo">
               </div>
               <div>
-                <a href="#" class="font-semibold text-gray-900">
-                  Engagement
+                <a href="https://jaga.id/jendela-pencegahan/spi" target="_blank" class="font-semibold text-gray-900">
+                  Informasi Skor SPI (Kementerian/Lembaga/Pemerintah Daerah)
                   <span class="absolute inset-0"></span>
                 </a>
-                <p class="mt-1 text-gray-600">Speak directly to your customers</p>
+                <p class="mt-1 text-gray-600">Survei Penilaian Integritas</p>
               </div>
             </div>
             <div class="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
-              <div class="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                <svg class="h-6 w-6 text-gray-600 group-hover:text-indigo-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M7.864 4.243A7.5 7.5 0 0119.5 10.5c0 2.92-.556 5.709-1.568 8.268M5.742 6.364A7.465 7.465 0 004.5 10.5a7.464 7.464 0 01-1.15 3.993m1.989 3.559A11.209 11.209 0 008.25 10.5a3.75 3.75 0 117.5 0c0 .527-.021 1.049-.064 1.565M12 10.5a14.94 14.94 0 01-3.6 9.75m6.633-4.596a18.666 18.666 0 01-2.485 5.33" />
-                </svg>
+              <div class="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                <img class="h-6 w-6 group-hover:text-indigo-600" src="{{ asset('img/logo-jaga-new.png') }}" alt="Logo">
               </div>
               <div>
-                <a href="#" class="font-semibold text-gray-900">
-                  Security
+                <a href="https://jaga.id/kuisprofit" target="_blank" class="font-semibold text-gray-900">
+                  Informasi Panduan Cegah Korupsi (PanCEK)
                   <span class="absolute inset-0"></span>
                 </a>
-                <p class="mt-1 text-gray-600">Your customers’ data will be safe and secure</p>
+                <p class="mt-1 text-gray-600">Informasi Panduan Cegah Korupsi</p>
               </div>
             </div>
             <div class="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
-              <div class="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                <svg class="h-6 w-6 text-gray-600 group-hover:text-indigo-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 002.25-2.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v2.25A2.25 2.25 0 006 10.5zm0 9.75h2.25A2.25 2.25 0 0010.5 18v-2.25a2.25 2.25 0 00-2.25-2.25H6a2.25 2.25 0 00-2.25 2.25V18A2.25 2.25 0 006 20.25zm9.75-9.75H18a2.25 2.25 0 002.25-2.25V6A2.25 2.25 0 0018 3.75h-2.25A2.25 2.25 0 0013.5 6v2.25a2.25 2.25 0 002.25 2.25z" />
-                </svg>
+              <div class="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                <img class="h-6 w-6 group-hover:text-indigo-600" src="{{ asset('img/logo_gol.png') }}" alt="Logo">
               </div>
               <div>
-                <a href="#" class="font-semibold text-gray-900">
-                  Integrations
+                <a href="https://gol.kpk.go.id/dokumen/" target="_blank" class="font-semibold text-gray-900">
+                  Dokumen Publik
                   <span class="absolute inset-0"></span>
                 </a>
-                <p class="mt-1 text-gray-600">Connect with third-party tools</p>
+                <p class="mt-1 text-gray-600">Dokumen Publik</p>
               </div>
             </div>
             <div class="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
-              <div class="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                <svg class="h-6 w-6 text-gray-600 group-hover:text-indigo-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
-                </svg>
+              <div class="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                <img class="h-6 w-6 group-hover:text-indigo-600" src="{{ asset('img/logo_gol.png') }}" alt="Logo">
               </div>
               <div>
-                <a href="#" class="font-semibold text-gray-900">
-                  Automations
+                <a href="https://gol.kpk.go.id/faq/" target="_blank" class="font-semibold text-gray-900">
+                  Frequently Asked Question Seputar Gratifikasi dan GOL 
                   <span class="absolute inset-0"></span>
                 </a>
-                <p class="mt-1 text-gray-600">Build strategic funnels that will convert</p>
+                <p class="mt-1 text-gray-600">Daftar pertanyaan yang sering diajukan beserta jawabannya</p>
               </div>
             </div>
           </div>
@@ -127,7 +116,7 @@
               <svg class="h-5 w-5 flex-none text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                 <path fill-rule="evenodd" d="M2 3.5A1.5 1.5 0 013.5 2h1.148a1.5 1.5 0 011.465 1.175l.716 3.223a1.5 1.5 0 01-1.052 1.767l-.933.267c-.41.117-.643.555-.48.95a11.542 11.542 0 006.254 6.254c.395.163.833-.07.95-.48l.267-.933a1.5 1.5 0 011.767-1.052l3.223.716A1.5 1.5 0 0118 15.352V16.5a1.5 1.5 0 01-1.5 1.5H15c-1.149 0-2.263-.15-3.326-.43A13.022 13.022 0 012.43 8.326 13.019 13.019 0 012 5V3.5z" clip-rule="evenodd" />
               </svg>
-              Contact sales
+              Contact Us
             </a>
           </div>
         </div>
@@ -141,3 +130,23 @@
     </a>
   </div>
 </nav>
+
+<style>
+.custom-scrollbar::-webkit-scrollbar {
+  width: 6px;
+}
+
+.custom-scrollbar::-webkit-scrollbar-track {
+  background: #f1f1f1;
+  border-radius: 10px;
+}
+
+.custom-scrollbar::-webkit-scrollbar-thumb {
+  background: #888;
+  border-radius: 10px;
+}
+
+.custom-scrollbar::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
+</style>
