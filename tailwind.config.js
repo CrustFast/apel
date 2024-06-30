3a1a3a5f75e852afdd299ca31307d1b007fb3ae3
simadmin <cimahi@TTUC7>
2024-06-30 08:41:03 +0700

    add: some themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,38 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        'body': [
+          'Inter', 
+          'ui-sans-serif', 
+          'system-ui', 
+          '-apple-system', 
+          'system-ui', 
+          'Segoe UI', 
+          'Roboto', 
+          'Helvetica Neue', 
+          'Arial', 
+          'Noto Sans', 
+          'sans-serif', 
+          'Apple Color Emoji', 
+          'Segoe UI Emoji', 
+          'Segoe UI Symbol', 
+          'Noto Color Emoji'
+        ],
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans, 
+        'ui-sans-serif', 
+        'system-ui', 
+        '-apple-system', 
+        'system-ui', 
+        'Segoe UI', 
+        'Roboto', 
+        'Helvetica Neue', 
+        'Arial', 
+        'Noto Sans', 
+        'sans-serif', 
+        'Apple Color Emoji', 
+        'Segoe UI Emoji', 
+        'Segoe UI Symbol', 
+        'Noto Color Emoji'],
       },
       colors: {
         'blue-bmti': '#208AEB',
@@ -20,6 +51,8 @@ export default {
   },
   plugins: [
     require('@tailwindcss/forms'),
-    require('flowbite/plugin'),
+    require('flowbite/plugin')({
+      charts: true,
+    }),
   ],
 }
