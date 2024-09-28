37db53ab05e5df32b11f1ebeb1dc6414416fdfb1
simadmin <cimahi@TTUC7>
2024-09-28 11:58:28 +0700

    fix home view

--- resources/views/components/apel.blade.php
+++ resources/views/components/apel.blade.php
@@ -222,12 +222,6 @@
   <main class="main-content" id="main-content">
     <div class="relative isolate overflow-hidden bg-white py-24 sm:py-32">
       <img src="{{ asset('img/bg bmti.png') }}" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center">
-      <div class="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl" aria-hidden="true">
-        <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
-      </div>
-      <div class="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu" aria-hidden="true">
-        <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
-      </div>
       <div class="flex flex-col items-center justify-center mx-auto max-w-7xl px-8 mt-5 lg:px-8">
         <div class="flex flex-col items-center justify-center mx-auto max-w-2xl lg:mx-0">
           <img src="{{ asset('img/logo-bmti.png') }}" alt="logo bmti" class="h-24 sm:h-28 md:h-28 lg:h-28 xl:h-28 w-auto mb-5" data-aos="zoom-in" data-aos-duration="1000">
