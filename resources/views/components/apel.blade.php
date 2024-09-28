e0de44ffef2d7db2411c7c68762543ed2ed78275
simadmin <cimahi@TTUC7>
2024-09-28 12:12:14 +0700

    change name KONFES

--- resources/views/components/apel.blade.php
+++ resources/views/components/apel.blade.php
@@ -177,7 +177,7 @@
                     KONFES
                     <span class="absolute inset-0"></span>
                   </a>
-                  <p class="mt-1 text-gray-600">Kontrol Benturan Kepentingan Sistematis</p>
+                  <p class="mt-1 text-gray-600">Sistem Kontrol Benturan Kepentingan</p>
                 </div>
               </div>
               <div class="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
@@ -296,7 +296,7 @@
                       <label for="job-2" class="inline-flex items-center justify-between w-full p-5 text-gray-900 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-500 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-900 hover:bg-gray-100 dark:text-white dark:bg-gray-600 dark:hover:bg-gray-500"  onclick="window.location='{{ route('benturan-kepentingan.view') }}'">
                         <div class="block">
                           <div class="w-full text-lg font-semibold">KONFES</div>
-                          <div class="w-full text-gray-500 dark:text-gray-400">Kontrol Benturan Kepentingan Sistematis</div>
+                          <div class="w-full text-gray-500 dark:text-gray-400">Sistem Kontrol Benturan Kepentingan</div>
                         </div>
                         <svg class="w-4 h-4 ms-3 rtl:rotate-180 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
